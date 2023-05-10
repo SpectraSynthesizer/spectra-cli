cb6bc46dd4f42dc683128c6196512bfa7726e664
Ilia Shevrin <iliashevrin@gmail.com>
2023-05-10 20:07:31 +0300

    update to version of May 2023

--- tau.smlab.syntech.Spectra.cli/spectra-cli-install.bat
+++ tau.smlab.syntech.Spectra.cli/spectra-cli-install.bat
@@ -1,7 +1,7 @@
 @echo off
 set SPECTRA_PATH=C:\Program Files (x86)\Spectra\
 if not exist "%SPECTRA_PATH%" mkdir "%SPECTRA_PATH%"
-xcopy lib\cudd.dll "%SPECTRA_PATH%" /R /Y
-xcopy lib\spectra-cli.jar "%SPECTRA_PATH%" /R /Y
+xcopy bin\cudd.dll "%SPECTRA_PATH%" /R /Y
+xcopy bin\spectra-cli.jar "%SPECTRA_PATH%" /R /Y
 setx /M PATH "%PATH%;%SPECTRA_PATH%"
 doskey spectra=java -jar "%SPECTRA_PATH%spectra-cli.jar" $* 
